da29dd7b97eb6eb2a201653322ce7fc8b468d6cd
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-06-21 04:04:09 +0530

    Updating batch file to try out requirements.txt fetch, need venv, out in next update

--- Run.bat
+++ Run.bat
@@ -3,3 +3,24 @@
 git pull
 
 start "" "_Helper.ipynb"
+
+@echo off
+
+@REM :: Install pip (if not already installed)
+@REM set python_ver=36
+@REM python ./get-pip.py
+@REM cd \
+@REM cd \python%python_ver%\Scripts\
+@REM pip install xlrd
+@REM pip install XlsxWriter
+@REM :: Pause to keep the window open
+@REM pause
+@REM exit
+
+@REM :: Create a requirements.txt file with the following content:
+@REM :: xlrd
+@REM :: XlsxWriter
+@REM :: tkinter
+
+@REM :: Install the modules from requirements.txt
+@REM pip install -r requirements.txt
