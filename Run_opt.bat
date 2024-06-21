3a245e8a77c22764bc12671e7e44c5f4a77ce6a9
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-06-22 01:37:13 +0530

    Fixed launch issues, opens .ipynb automatically

--- Run_opt.bat
+++ Run_opt.bat
@@ -3,39 +3,44 @@ setlocal
 
 :: Check if venv_sdh exists
 if not exist venv_sdh (
-    echo Creating venv...
+    echo %time% Creating venv...
     python -m venv venv_sdh
 )
 
 :: Deactive venv
 call venv_sdh\Scripts\deactivate.bat
 
-
 :: Activate venv
-echo Activating venv...
+echo %time% Activating venv...
 call venv_sdh\Scripts\activate
 
 :: Pull from Git
-echo Pulling from git...
+echo %time% Pulling from git...
 git pull
 
 :: Update requirements
-echo Updating requirements...
-pip install -r requirements.txt
+echo %time% Updating requirements...
+pip install -r requirements.txt > nul 2>&1
 
-:: Launch _Helper.ipynb with a specific kernel (e.g., "python3")
-echo Launching .ipynb...
-call jupyter notebook _Helper.ipynb --NotebookApp.kernel_name=python3
-
-:: Deactivate venv
-deactivate
-
-:: Add a loading bar (optional)
-for /l %%A in (1,1,10) do (
-    <nul set /p ".=."
-    ping -n 1 127.0.0.1 > nul
+echo %time% Updating/Checking
+for /l %%i in (1,1,10) do (
+    <nul set /p "=."
+    ping localhost -n 2 > nul
 )
 echo.
 
+:: Launch _Helper.ipynb with a specific kernel (e.g., "python3")
+echo %time% Launching .ipynb...
+echo .ipynb launched at http://localhost:8888/notebooks/_Helper.ipynb
+call jupyter notebook _Helper.ipynb --NotebookApp.kernel_name=python3 > nul 2>&1
+
+
+:: Deactivate venv
+echo %time% Deactivating venv...
+deactivate
+
+echo %time% Loading complete.
+
 :: Pause to keep the window open
+echo %time% Pausing execution...
 pause
