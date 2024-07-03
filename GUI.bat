da5382ed38bbfc3cdaf4982165cf9105d7d5fac9
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-07-03 07:40:41 +0530

    Testing GUI's batch file

---
+++ GUI.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal
+
+:: Check if venv_sdh exists
+if not exist venv_sdh (
+    echo %time% Creating venv...
+    python -m venv venv_sdh
+)
+
+:: Deactive venv
+call venv_sdh\Scripts\deactivate.bat
+
+:: Activate venv
+echo %time% Activating venv...
+call venv_sdh\Scripts\activate
+
+echo %time% Updating Repository
+git add *
+git stash
+git pull > nul 2>&1
+
+echo %time% Updating/Checking
+for /l %%i in (1,1,10) do (
+    <nul set /p "=."
+    ping localhost -n 2 > nul
+)
+echo.
+
+:: Launch _Helper.ipynb with a specific kernel (e.g., "python3")
+echo %time% Launching GUI...
+python gradio_GUI.py
+
+:: start "" "_Helper.ipynb"
+
+@echo off
+
+:: test test
