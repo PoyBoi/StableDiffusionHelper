41b6ac0ebfa5543fb97a6bb1d39fe87590235bc3
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-07-07 12:39:53 +0530

    Updated init to account for code removal

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,8 +1,8 @@
 from .caption_list import get_most_common_words
 from .delete_words import process_files
 from .gradio_code import f, change_tab, ratio_check, get_files, check_files, set2zero
-from .img_processing import  convert_images_to_png, remove_black_bars, process_images, select_best_images, detect_faces_and_evaluate, remove_background_from_images, main_call, faceCrop, rectangularCrop, detect_anime
+from .img_processing import  convert_images_to_png, remove_black_bars, process_images, select_best_images, detect_faces_and_evaluate, remove_background_from_images, main_call, detect_anime
 
 __all__ = [
-    get_most_common_words, process_files, convert_images_to_png, remove_black_bars, process_images, select_best_images, detect_faces_and_evaluate, main_call, faceCrop, rectangularCrop, detect_anime, f, change_tab, ratio_check, get_files, check_files, set2zero
+    get_most_common_words, process_files, convert_images_to_png, remove_black_bars, process_images, select_best_images, detect_faces_and_evaluate, main_call, detect_anime, f, change_tab, ratio_check, get_files, check_files, set2zero
 ]
